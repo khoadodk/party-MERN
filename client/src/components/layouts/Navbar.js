74a60abccf234003a7a082c2de185560166421b3
Khoa Do <khoado.dk@gmail.com>
2019-12-26 15:24:57 -0500

    remove submodule

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1>
+          Party<i class="fas fa-glass-cheers"></i>
+        </h1>
+      </div>
+
+      <ul>
+        <li>Hello, Khoa</li>
+        <li>
+          <a href="#">
+            Log Out<i className="fas fa-sign-out-alt"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
